72aab77f703c2fb80d7c847dde164b0a6852d383
Kilros0817 <lazarkitfrl@gmail.com>
2025-05-07 10:45:36 +0800

    remove sqlite3 db

--- news/settings.py
+++ news/settings.py
@@ -75,10 +75,6 @@ WSGI_APPLICATION = 'news.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 
